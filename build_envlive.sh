8f926fd1b8d3b662049e8deddaef4c5b62a9fffc
Alexis Cassaigne <alexis@cassaigne.gmail.com>
2015-10-10 02:32:55 +0200

    Some fixes after, it is almost working.

--- build_envlive.sh
+++ build_envlive.sh
@@ -4,105 +4,124 @@
 source ./config.sh
 
 # Preventively unmounting filesystems
-umount -R ${PROLOLIVE_DIR}/boot
-umount -R ${PROLOLIVE_DIR}
-umount -R overlay-intermediate
-umount -R ${PROLOLIVE_DIR}.light
-umount -R ${PROLOLIVE_DIR}.big
-umount -R ${PROLOLIVE_DIR}.full
-
-
-# Deleting previously generated squash files
-rm -f ${PROLOLIVE_DIR}*.squashfs
+echo -n "Unmounting previously mounted filesystems, if any..."
+umount -R ${PROLOLIVE_DIR}/boot  2>/dev/null
+umount -R ${PROLOLIVE_DIR}       2>/dev/null
+umount -R overlay-intermediate   2>/dev/null
+umount -R ${PROLOLIVE_DIR}.light 2>/dev/null
+umount -R ${PROLOLIVE_DIR}.big   2>/dev/null
+umount -R ${PROLOLIVE_DIR}.full  2>/dev/null
+echo " Done."
 
 
 # Allocating space for filesystems and the root
+echo -n "Allocating filesystems files..."
 rm -f ${PROLOLIVE_IMG}.light && fallocate -l 1G            ${PROLOLIVE_IMG}.light
 rm -f ${PROLOLIVE_IMG}.big   && fallocate -l 1500M         ${PROLOLIVE_IMG}.big
 rm -f ${PROLOLIVE_IMG}.full  && fallocate -l 5G            ${PROLOLIVE_IMG}.full
 rm -f ${PROLOLIVE_IMG}       && fallocate -l ${IMAGE_SIZE} ${PROLOLIVE_IMG}
-
+echo " Done."
 
 # Partitionning the image disk file
+echo -n "Partitionning the disk image..."
 sfdisk ${PROLOLIVE_IMG} < prololive.dos
-
+echo " Done."
 
 # Creating loop devices for the disk image file
+echo -n "Generating device mappings for the disk image..."
 LOOP=$(kpartx -l ${PROLOLIVE_IMG} | grep -o "loop[0-9]" | head -n1)
 rm -f /dev/mapper/${LOOP}
 kpartx -as ${PROLOLIVE_IMG}
+echo " Done."
 
 # Formatting all filesystems
-
+echo -n "Formatting all filesystems..."
 for f in ${PROLOLIVE_IMG}.full ${PROLOLIVE_IMG}.big ${PROLOLIVE_IMG}.light ; do
-    mkfs.ext4 -F $f
+    mkfs.ext4 -q -F $f
 done
 
-mkfs.ext4 /dev/mapper/${LOOP}p1 -L proloboot
-mkfs.ext4 /dev/mapper/${LOOP}p2 -L persistent
-
+mkfs.ext4 -q -F /dev/mapper/${LOOP}p1 -L proloboot
+mkfs.ext4 -q -F /dev/mapper/${LOOP}p2 -L persistent
+echo " Done."
 
 # Mounting filesystems
+echo -n "Mounting filesystems and adding working/ and system/ directories..."
 mkdir -p ${PROLOLIVE_DIR}/      # Where all FS will be union-mounted
 mkdir -p ${PROLOLIVE_DIR}.light # Where the core OS filesystem will be mounted alone
-mkdir -p ${PROLOLIVE_DIR}.big   # Where many light toold will be provided as well as DE/WP
+mkdir -p ${PROLOLIVE_DIR}.big   # Where many light tools will be provided as well as DE/WP
 mkdir -p ${PROLOLIVE_DIR}.full  # Where the biggest packages will go. Hi ghc!
+mkdir -p overlay-intermediate
 
 mount ${PROLOLIVE_IMG}.light ${PROLOLIVE_DIR}.light
 mount ${PROLOLIVE_IMG}.big   ${PROLOLIVE_DIR}.big
 mount ${PROLOLIVE_IMG}.full  ${PROLOLIVE_DIR}.full
 
 for mountpoint in ${PROLOLIVE_DIR}.light ${PROLOLIVE_DIR}.big ${PROLOLIVE_DIR}.full ; do
-    mkdir -p ${mountpoint}/{work,system} # Creating them for harmonization when making squashfs
+    mkdir ${mountpoint}/{work,system} # Creating them for harmonization when making squashfs
 done
 
+mount ${PROLOLIVE_IMG}.light ${PROLOLIVE_DIR}.light
+mount ${PROLOLIVE_IMG}.big ${PROLOLIVE_DIR}.big
+mount ${PROLOLIVE_IMG}.full ${PROLOLIVE_DIR}.full
 mount -t overlay overlay -o lowerdir=${PROLOLIVE_DIR}.light/system,upperdir=${PROLOLIVE_DIR}.big/system/,workdir=${PROLOLIVE_DIR}.big/work/ overlay-intermediate/
 mount -t overlay overlay -o lowerdir=overlay-intermediate,upperdir=${PROLOLIVE_DIR}.full/system/,workdir=${PROLOLIVE_DIR}.full/work/ ${PROLOLIVE_DIR}
 
 mkdir ${PROLOLIVE_DIR}/boot
 mount /dev/mapper/${LOOP}p1 ${PROLOLIVE_DIR}/boot
+echo " Done."
 
 
-# Installing core system packages on the lower layer
-pacstrap -C pacman.conf -c ${PROLOLIVE_DIR}.light/system base base-devel
+echo "Installing packages available in repositories..."
+echo "Installing core system packages on the lower layer"
+pacstrap -d -C pacman.conf -c ${PROLOLIVE_DIR}.light/system base base-devel
+mount -o remount,ro ${PROLOLIVE_IMG}.light
 
-# Installing some not-too-big packages on the middle layer (overlay-intermediate)
+
+echo "Installing some not-too-big packages on the middle layer (overlay-intermediate)"
 pacstrap -C pacman.conf -c overlay-intermediate/ btrfs-progs clang firefox \
 	 firefox-i18n-fr grml-zsh-config htop networkmanager openssh \
 	 rxvt-unicode screen tmux zsh ntfs-3g lxqt xorg xorg-apps gdb valgrind \
 	 js luajit nodejs ocaml php
+mount -o remount,ro ${PROLOLIVE_IMG}.big
 
-# Installing the biggest packages on the top layer (${PROLOLIVE_DIR})
+echo "Installing the biggest packages on the top layer (${PROLOLIVE_DIR})"
 pacstrap -C pacman.conf -c ${PROLOLIVE_DIR}/ clang-analyzer clang-tools-extra \
 	 git mercurial ntp reptyr rlwrap rsync samba syslinux wget \
 	 sublime-text codeblocks eclipse ed eric eric-i18n-fr geany kate \
 	 kdevelop leafpad mono-debugger monodevelop monodevelop-debugger-gdb \
 	 netbeans openjdk8-doc scite boost ghc
 
-
 # Copying the hook needed to mount squash filesystems on boot
+echo -n "Copying hook files..."
 cp prolomount.build ${PROLOLIVE_DIR}/usr/lib/initcpio/install/prolomount
 cp prolomount.runtime ${PROLOLIVE_DIR}/usr/lib/initcpio/hooks/prolomount
+echo " Done."
 
 
 # Copying config and generating initcpio
+echo "Generating the initcpio ramdisk..."
 cp mkinitcpio.conf ${PROLOLIVE_DIR}/etc/mkinitcpio.conf
 systemd-nspawn -q -D ${PROLOLIVE_DIR} mkinitcpio -p linux
-
+echo "Done."
 
 # Configuring passwords for prologin and root users
+echo -n "Configuring users and passwords..."
 systemd-nspawn -q -D ${PROLOLIVE_DIR} usermod root -p $(echo ${CANONIQUE} | openssl passwd -1 -stdin) -s /bin/zsh
 systemd-nspawn -q -D ${PROLOLIVE_DIR} useradd prologin -G games -m -p $(echo "prologin" | openssl passwd -1 -stdin) -s /bin/zsh
+echo " Done."
+
 
 # Copying the pacman config
 cp pacman.conf ${PROLOLIVE_DIR}/etc/pacman.conf
 
 
 # Installing yaourt and some AUR packages
+echo "Installing some AUR packages..."
 systemd-nspawn -q -D ${PROLOLIVE_DIR} pacman -S yaourt --noconfirm
 echo "prologin ALL=(ALL) NOPASSWD: ALL" >> ${PROLOLIVE_DIR}/etc/sudoers
 systemd-nspawn -q -D ${PROLOLIVE_DIR} -u prologin yaourt -S notepadqq-git pycharm-community --noconfirm
 sed "s:prologin:#prologin:" -i ${PROLOLIVE_DIR}/etc/sudoers
+echo "Done."
 
 # Configuring fstab
 cat > ${PROLOLIVE_DIR}/etc/fstab <<EOF
@@ -113,7 +132,9 @@ tmpfs           /home/prologin/.cache     tmpfs defaults 0 0
 tmpfs           /home/prologin/ramfs      tmpfs defaults 0 0
 EOF
 
+
 # Configuring system environment
+echo "Doing some configuration..."
 echo "prololive" > ${PROLOLIVE_DIR}/etc/hostname
 systemd-nspawn -q -D ${PROLOLIVE_DIR} ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 echo 'fr_FR.UTF-8' >  ${PROLOLIVE_DIR}/etc/locale.gen
@@ -131,29 +152,32 @@ EOF
 
 # Creating dirs who will be ramfs-mounted
 systemd-nspawn -q -D ${PROLOLIVE_DIR} -u prologin mkdir /home/prologin/.cache /home/prologin/ramfs
-
+echo "Done."
 
 # Configuring boot system
-mkdir ${PROLOLIVE_DIR}/boot/syslinux
+echo "Installing syslinux..."
+mkdir -p ${PROLOLIVE_DIR}/boot/syslinux
 cp -r ${PROLOLIVE_DIR}/usr/lib/syslinux/bios/*.c32 ${PROLOLIVE_DIR}/boot/syslinux/
-extlinux --install ${PROLOLIVE_DIR}/boot/syslinux
+extlinux --install ${PROLOLIVE_DIR}/boot/syslinux || echo "SOMETHING WENT WRONG."
 dd if=${PROLOLIVE_DIR}/usr/lib/syslinux/bios/mbr.bin of=${PROLOLIVE_IMG} bs=440 count=1
 
-cp ${LOGOFILE} ${PROLOLIVE_DIR}/boot/syslinux/${LOGOFILE}
+cp ${LOGOFILE} ${PROLOLIVE_DIR}/boot/syslinux/${LOGOFILE} || echo -n " missing ${LOGOFILE} file..."
 cp syslinux.cfg ${PROLOLIVE_DIR}/boot/syslinux/
-
-umount ${PROLOLIVE_DIR}/boot
-
+echo " Done."
 
 # Creating squash filesystems
+echo "Creating squash filesystems..."
 for mountpoint in ${PROLOLIVE_DIR}.light ${PROLOLIVE_DIR}.big ${PROLOLIVE_DIR}.full ; do
-    mksquashfs ${mountpoint}/system ${mountpoint}.squashfs -comp lz4 -b 1048576 -e ${mountpoint}/system/proc ${mountpoint}/system/tmp ${mountpoint}/system/home ${mountpoint}/system/boot ${mountpoint}/system/dev
+    mksquashfs ${mountpoint}/system ${PROLOLIVE_DIR}/boot/${mountpoint}.squashfs -comp lz4 -b 1048576 -e ${mountpoint}/system/proc ${mountpoint}/system/tmp ${mountpoint}/system/home ${mountpoint}/system/boot ${mountpoint}/system/dev
 done
 
-
 # Unmounting all mounted filesystems
-umount ${PROLOLIVE_DIR}       # All FS merged minus /boot
+echo -n "Unmounting filesystems..."
+umount -R ${PROLOLIVE_DIR}       # All FS merged minus /boot
 umount ${PROLOLIVE_DIR}.full  #
 umount overlay-intermediate   # Light and big FS merged
 umount ${PROLOLIVE_DIR}.big   #
 umount ${PROLOLIVE_DIR}.light #
+echo " Done."
+
+echo "The end."
