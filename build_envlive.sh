01e76c554cf7682d4d172c001aba3f53904ddc2d
Alexis Cassaigne <alexis@cassaigne.gmail.com>
2015-11-04 19:26:35 +0100

    build_envlive.sh: rewrite messages and comments; README and pacman.conf: completions and fixes (wow).

--- build_envlive.sh
+++ build_envlive.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Loading the config
+# Load the config
 source ./config.sh
 
-# Preventively unmounting filesystems
+# Preventively unmount filesystems
 echo -n "Unmounting previously mounted filesystems, if any..."
 umount -R ${PROLOLIVE_DIR}/boot  2>/dev/null
 umount -R ${PROLOLIVE_DIR}       2>/dev/null
@@ -16,39 +16,37 @@ kpartx -r ${PROLOLIVE_IMG}       2>/dev/null
 echo " Done."
 
 
-# Allocating space for filesystems and the root
-echo -n "Allocating filesystems files (will not overwrite existing files"
+# Allocate space for filesystems and the root
+echo "Allocating filesystems files (will not overwrite existing files)..."
 (test -f ${PROLOLIVE_IMG}.light && dd if=/dev/zero of=${PROLOLIVE_IMG}.light bs=1M count=1 conv=notrunc) || dd if=/dev/zero of=${PROLOLIVE_IMG}.light bs=1M count=1 seek=1000
 (test -f ${PROLOLIVE_IMG}.big   && dd if=/dev/zero of=${PROLOLIVE_IMG}.big   bs=1M count=1 conv=notrunc) || dd if=/dev/zero of=${PROLOLIVE_IMG}.big   bs=1M count=1 seek=2500
 (test -f ${PROLOLIVE_IMG}.full  && dd if=/dev/zero of=${PROLOLIVE_IMG}.full  bs=1M count=1 conv=notrunc) || dd if=/dev/zero of=${PROLOLIVE_IMG}.full  bs=1M count=1 seek=5000
 test -f ${PROLOLIVE_IMG}                                                                                 || dd if=/dev/zero of=${PROLOLIVE_IMG}       bs=1M count=1 seek=3823
-echo " Done."
+echo "Done."
 
-# Partitionning the image disk file
-echo -n "Partitionning the disk image..."
+# Partition the image disk file
+echo "Partition the disk image"
 sfdisk ${PROLOLIVE_IMG} < prololive.dos
-echo " Done."
 
-# Creating loop devices for the disk image file
-echo -n "Generating device mappings for the disk image..."
+# Create loop devices for the disk image file
+echo "Generate device mappings for the disk image..."
 LOOP=$(kpartx -av ${PROLOLIVE_IMG} | grep -o "loop[0-9]" | tail -n1)
 # To avoid annoying messages about already existing filesystems.
 dd if=/dev/zero of=/dev/mapper/${LOOP}p1 bs=1M count=1
 dd if=/dev/zero of=/dev/mapper/${LOOP}p2 bs=1M count=1
-echo " Done."
+echo "Done."
 
-# Formatting all filesystems
-echo -n "Formatting all filesystems..."
+# Format all filesystems
+echo "Format all filesystems..."
 for f in ${PROLOLIVE_IMG}.full ${PROLOLIVE_IMG}.big ${PROLOLIVE_IMG}.light ; do
-    mkfs.ext4 -q $f
+    mkfs.ext4 $f
 done
 
-mkfs.ext4 -F /dev/mapper/${LOOP}p1 -L proloboot
-mkfs.ext4 -F /dev/mapper/${LOOP}p2 -L persistent
-echo " Done."
+mkfs.ext4 /dev/mapper/${LOOP}p1 -L proloboot
+mkfs.ext4 /dev/mapper/${LOOP}p2 -L persistent
 
-# Mounting filesystems
-echo -n "Mounting filesystems and adding working/ and system/ directories..."
+# Mount filesystems
+echo -n "Mounting filesystems and adding working/ and system/ directories in each one..."
 mkdir -p ${PROLOLIVE_DIR}/      # Where all FS will be union-mounted
 mkdir -p ${PROLOLIVE_DIR}.light # Where the core OS filesystem will be mounted alone
 mkdir -p ${PROLOLIVE_DIR}.big   # Where many light tools will be provided as well as DE/WP
@@ -61,13 +59,13 @@ mount ${PROLOLIVE_IMG}.big   ${PROLOLIVE_DIR}.big
 mount ${PROLOLIVE_IMG}.full  ${PROLOLIVE_DIR}.full
 
 for mountpoint in ${PROLOLIVE_DIR}.light ${PROLOLIVE_DIR}.big ${PROLOLIVE_DIR}.full ; do
-    mkdir ${mountpoint}/{work,system} # Creating all of them for harmonization when making squashfs
+    mkdir ${mountpoint}/{work,system} # Create all of them for harmonization when making squashfs
 done
 echo " Done."
 
 
-echo "Installing packages available in repositories..."
-echo "Installing core system packages on the lower layer"
+echo "Install packages available in repositories..."
+echo "Install core system packages on the lower layer"
 # Binding directory to a mountpoint is a good way to ensure a good pacstrap behaviour
 mount -o bind ${PROLOLIVE_DIR}.light/system first-bind/
 mkdir -p first-bind/boot
@@ -75,21 +73,20 @@ mount /dev/mapper/${LOOP}p1 first-bind/boot
 pacstrap -C pacman.conf -c first-bind base base-devel syslinux
 
 
-# Copying the hook needed to mount squash filesystems on boot
+# Copy the hook needed to mount squash filesystems on boot
 echo -n "Copying hook files..."
 cp prolomount.build first-bind/usr/lib/initcpio/install/prolomount
 cp prolomount.runtime first-bind/usr/lib/initcpio/hooks/prolomount
 echo " Done."
 
 
-# Copying config and generating initcpio
-echo "Generating the initcpio ramdisk..."
+# Copy config and generating initcpio
+echo "Generate the initcpio ramdisk..."
 cp mkinitcpio.conf first-bind/etc/mkinitcpio.conf
 systemd-nspawn -q -D first-bind mkinitcpio -p linux
-echo "Done."
 
 
-echo "Installing interpreters (GHC apart) and graphical packages on the intermediate layer"
+echo "Install interpreters (GHC apart) and graphical packages on the intermediate layer"
 umount /dev/mapper/${LOOP}p1
 umount first-bind
 mount -t overlay overlay -o lowerdir=${PROLOLIVE_DIR}.light/system,upperdir=${PROLOLIVE_DIR}.big/system,workdir=${PROLOLIVE_DIR}.big/work overlay-intermediate/
@@ -103,7 +100,7 @@ pacstrap -C pacman.conf -c overlay-intermediate/ boost ed firefox firefox-i18n-f
 umount /dev/mapper/${LOOP}p1
 umount overlay-intermediate
 
-echo "Installing the biggest packages on the top layer (${PROLOLIVE_DIR})"
+echo "Install remaining and big packages on the top layer (${PROLOLIVE_DIR})"
 mount -t overlay overlay -o lowerdir=${PROLOLIVE_DIR}.big/system:${PROLOLIVE_DIR}.light/system,upperdir=${PROLOLIVE_DIR}.full/system/,workdir=${PROLOLIVE_DIR}.full/work/ ${PROLOLIVE_DIR}
 mount /dev/mapper/${LOOP}p1 ${PROLOLIVE_DIR}/boot
 pacstrap -C pacman.conf -c ${PROLOLIVE_DIR}/ codeblocks eclipse eric \
@@ -112,11 +109,10 @@ pacstrap -C pacman.conf -c ${PROLOLIVE_DIR}/ codeblocks eclipse eric \
 	 notepadqq-bin openjdk7-doc pycharm-community reptyr rsync \
 	 samba scite
 
-# Configuring system environment
-echo "Doing some configuration..."
+# Configure system environment
+echo "System configuration..."
 echo "prololive" > ${PROLOLIVE_DIR}/etc/hostname
 systemd-nspawn -q -D ${PROLOLIVE_DIR} ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
-#systemd-nspawn -q -D ${PROLOLIVE_DIR} systemctl mask systemd-journald.service
 echo 'fr_FR.UTF-8 UTF-8' >  ${PROLOLIVE_DIR}/etc/locale.gen
 echo 'en_US.UTF-8 UTF-8' >> ${PROLOLIVE_DIR}/etc/locale.gen
 echo "LANG=fr_FR.UTF-8"  >  ${PROLOLIVE_DIR}/etc/locale.conf
@@ -135,18 +131,18 @@ cat > ${PROLOLIVE_DIR}/etc/systemd/journald.conf <<EOF
 Storage=volatile
 EOF
 
-# Configuring passwords for prologin and root users
+# Configure passwords for prologin and root users
 echo -n "Configuring users and passwords..."
 systemd-nspawn -q -D ${PROLOLIVE_DIR} usermod root -p $(echo ${CANONIQUE} | openssl passwd -1 -stdin) -s /bin/zsh
 systemd-nspawn -q -D ${PROLOLIVE_DIR} useradd prologin -G games -m -p $(echo "prologin" | openssl passwd -1 -stdin) -s /bin/zsh
 echo " Done."
 
 
-# Copying the pacman config
+# Copy the pacman config
 cp pacman.conf ${PROLOLIVE_DIR}/etc/pacman.conf
 
 
-# Configuring fstab
+# Configure fstab
 cat > ${PROLOLIVE_DIR}/etc/fstab <<EOF
 LABEL=proloboot /boot                     ext4  defaults 0 0
 tmpfs           /var/cache/pacman         tmpfs defaults 0 0
@@ -155,11 +151,10 @@ tmpfs           /home/prologin/ramfs      tmpfs defaults 0 0
 EOF
 
 
-# Creating dirs who will be ramfs-mounted
+# Create dirs who will be ramfs-mounted
 systemd-nspawn -q -D ${PROLOLIVE_DIR} -u prologin mkdir /home/prologin/.cache /home/prologin/ramfs
-echo "Done."
 
-# Configuring boot system
+# Configure boot system
 echo -n "Installing syslinux..."
 dd if=${PROLOLIVE_DIR}/usr/lib/syslinux/bios/mbr.bin of=/dev/${LOOP} bs=440 count=1
 mkdir -p ${PROLOLIVE_DIR}/boot/syslinux
@@ -171,7 +166,7 @@ cp syslinux.cfg ${PROLOLIVE_DIR}/boot/syslinux/
 echo " Done."
 
 # Creating squash filesystems
-echo "Creating squash filesystems..."
+echo "Create squash filesystems..."
 for mountpoint in ${PROLOLIVE_DIR}.light ${PROLOLIVE_DIR}.big ${PROLOLIVE_DIR}.full ; do
     mksquashfs ${mountpoint}/system ${PROLOLIVE_DIR}/boot/${mountpoint}.squashfs -comp xz -Xdict-size 100% -b 1048576 -e ${mountpoint}/system/proc ${mountpoint}/system/tmp ${mountpoint}/system/boot ${mountpoint}/system/dev
 done
