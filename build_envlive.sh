3b7ef2358fb9470183647c78ee1998a6cd0644c8
Alexis Cassaigne <alexis@cassaigne.gmail.com>
2015-11-10 20:07:50 +0100

    build_envlive.sh: more verbose, one path bug fixed.

--- build_envlive.sh
+++ build_envlive.sh
@@ -21,7 +21,7 @@ echo " Done."
 # Allocate space for filesystems and the root
 echo "Allocating filesystems files (will not overwrite existing files)..."
 rm -f ${PROLOLIVE_IMG}
-dd if=/dev/zero of=${PROLOLIVE_IMG} bs=1M count=3824
+dd if=/dev/zero of=${PROLOLIVE_IMG} bs=1M seek=3823 count=1
 echo "Done."
 
 # Partition the image disk file
@@ -61,15 +61,15 @@ pacstrap -C pacman.conf -c ${ROOT} base base-devel syslinux
 
 # Copy the hook needed to mount squash filesystems on boot
 echo -n "Copy hook files..."
-cp prolomount.build ${ROOT}/usr/lib/initcpio/install/prolomount
-cp prolomount.runtime ${ROOT}/usr/lib/initcpio/hooks/prolomount
+cp -v prolomount.build ${ROOT}/usr/lib/initcpio/install/prolomount
+cp -v prolomount.runtime ${ROOT}/usr/lib/initcpio/hooks/prolomount
 echo " Done."
 
 
 # Copy config and generating initcpio
 echo "Generate the initcpio ramdisk..."
-cp mkinitcpio.conf ${ROOT}/etc/mkinitcpio.conf
-systemd-nspawn -q -D ${ROOT} mkinitcpio -p linux
+cp -v mkinitcpio.conf ${ROOT}/etc/mkinitcpio.conf
+systemd-nspawn -D ${ROOT} mkinitcpio -p linux
 
 umount /dev/mapper/${LOOP}p1
 umount ${ROOT}
@@ -102,17 +102,17 @@ pacstrap -C pacman.conf -c ${ROOT}/ codeblocks eclipse eric \
 # Configure system environment
 echo "System configuration..."
 echo "prololive" > ${ROOT}/etc/hostname
-systemd-nspawn -q -D ${ROOT} ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+systemd-nspawn -D ${ROOT} ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 echo 'fr_FR.UTF-8 UTF-8' >  ${ROOT}/etc/locale.gen
 echo 'en_US.UTF-8 UTF-8' >> ${ROOT}/etc/locale.gen
 echo "LANG=fr_FR.UTF-8"  >  ${ROOT}/etc/locale.conf
 echo "KEYMAP=fr-pc"      >  ${ROOT}/etc/vconsole.conf
-systemd-nspawn -q -D ${ROOT} locale-gen
-cp sddm.conf ${ROOT}/etc/
-systemd-nspawn -q -D ${ROOT} systemctl enable sddm
-systemd-nspawn -q -D ${ROOT} systemctl enable NetworkManager
-cp 00-keyboard.conf ${ROOT}/etc/X11/xorg.conf.d/
-cp .Xresources ${ROOT}/etc/skel/
+systemd-nspawn -D ${ROOT} locale-gen
+cp -v sddm.conf ${ROOT}/etc/
+systemd-nspawn -D ${ROOT} systemctl enable sddm
+systemd-nspawn -D ${ROOT} systemctl enable NetworkManager
+cp -v 00-keyboard.conf ${ROOT}/etc/X11/xorg.conf.d/
+cp -v .Xresources ${ROOT}/etc/skel/
 echo 'alias ocaml="rlwrap ocaml"' >> /etc/skel/.zshrc
 echo 'source /etc/profile.d/jre.sh' >> /etc/skel/.zshrc
 
@@ -123,13 +123,13 @@ EOF
 
 # Configure passwords for prologin and root users
 echo -n "Configuring users and passwords..."
-systemd-nspawn -q -D ${ROOT} usermod root -p $(echo ${CANONIQUE} | openssl passwd -1 -stdin) -s /bin/zsh
-systemd-nspawn -q -D ${ROOT} useradd prologin -G games -m -p $(echo "prologin" | openssl passwd -1 -stdin) -s /bin/zsh
+systemd-nspawn -D ${ROOT} usermod root -p $(echo ${CANONIQUE} | openssl passwd -1 -stdin) -s /bin/zsh
+systemd-nspawn -D ${ROOT} useradd prologin -G games -m -p $(echo "prologin" | openssl passwd -1 -stdin) -s /bin/zsh
 echo " Done."
 
 
 # Copy the pacman config
-cp pacman.conf ${ROOT}/etc/pacman.conf
+cp -v pacman.conf ${ROOT}/etc/pacman.conf
 
 
 # Configure fstab
@@ -141,17 +141,17 @@ EOF
 
 
 # Create dirs who will be ramfs-mounted
-systemd-nspawn -q -D ${ROOT} -u prologin mkdir /home/prologin/.cache /home/prologin/ramfs
+systemd-nspawn -D ${ROOT} -u prologin mkdir /home/prologin/.cache /home/prologin/ramfs
 
 # Configure boot system
 echo -n "Installing syslinux..."
 dd if=${ROOT}/usr/lib/syslinux/bios/mbr.bin of=/dev/${LOOP} bs=440 count=1
 mkdir -p ${ROOT}/boot/syslinux
-cp -r ${ROOT}/usr/lib/syslinux/bios/*.c32 ${ROOT}/boot/syslinux/
+cp -vr ${ROOT}/usr/lib/syslinux/bios/*.c32 ${ROOT}/boot/syslinux/
 extlinux --device /dev/mapper/${LOOP}p1 --install ${ROOT}/boot/syslinux
 
-cp logo.png ${ROOT}/boot/syslinux/ || echo -n " missing logo.png file..."
-cp syslinux.cfg ${ROOT}/boot/syslinux/
+cp -v logo.png ${ROOT}/boot/syslinux/ || echo -n " missing logo.png file..."
+cp -v syslinux.cfg ${ROOT}/boot/syslinux/
 echo " Done."
 
 umount ${ROOT}/boot
@@ -159,8 +159,8 @@ umount ${ROOT}
 
 ROOT=${PROLOLIVE_DIR}
 
-mount /dev/mapper/${LOOP}p1 ${ROOT}
-cp documentation.squashfs ${ROOT}/boot/
+mount /dev/mapper/${LOOP}p1 ${ROOT} || exit 42
+cp -v documentation.squashfs ${ROOT}/
 
 
 # Creating squash filesystems
