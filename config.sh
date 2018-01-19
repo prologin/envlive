2434bf11cfca5321875cf80490e256bbad800e31
Victor "multun" Collod <victor.collod@epita.fr>
2018-01-19 01:59:18 +0100

    config: update package list

--- config.sh
+++ config.sh
@@ -24,16 +24,14 @@ packages_base=( base base-devel syslinux )
 
 packages_intermediate=( boost ed firefox firefox-i18n-fr fpc \
 	 gambit-c gcc-ada gdb git grml-zsh-config htop jdk7-openjdk \
-	 lxqt-common lxqt-config lxqt-panel lxqt-policykit lxqt-qtplugin \
-	 lxqt-runner lxqt-session openbox oxygen-icons pcmanfm-qt luajit mono \
-	 mono-basic mono-debugger nodejs ntp ntfs-3g ocaml openssh php python \
+	 lxqt luajit mono \
+	 nodejs ntp ntfs-3g ocaml openssh php python \
 	 python2 qtcreator rlwrap rxvt-unicode screen sddm tmux ttf-dejavu \
 	 valgrind wget xorg xf86-video-intel xorg-apps zsh vim emacs \
 	 networkmanager network-manager-applet xterm zeal )
 
 packages_big=( codeblocks eclipse-java eclipse-ecj eric \
-	 eric-i18n-fr fsharp geany ghc leafpad monodevelop \
-	 monodevelop-debugger-gdb netbeans  openjdk7-doc \
-	 reptyr rsync samba scite )
+	 eric-i18n-fr geany ghc leafpad netbeans  openjdk7-doc \
+	 reptyr rsync samba pycharm-community-edition )
 
-packages_aur=( esotope-bfc-git notepadqq pycharm-community sublime-text )
+packages_aur=( esotope-bfc-git sublime-text-dev )
