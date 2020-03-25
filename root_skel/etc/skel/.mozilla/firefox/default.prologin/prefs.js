0454f1e5654f57ae305becaddad297d0b2121a21
Alexandre Iooss <erdnaxe@crans.org>
2020-03-25 18:04:25 +0100

    [Firefox] Use real JavaScript comments
    `#` does not look right for JavaScript.

--- root_skel/etc/skel/.mozilla/firefox/default.prologin/prefs.js
+++ root_skel/etc/skel/.mozilla/firefox/default.prologin/prefs.js
@@ -1,27 +1,27 @@
-# Hide some prompts
+// Hide some prompts
 user_pref("browser.customizemode.tip0.shown", true);
 user_pref("reader.parse-on-load.enabled", false);
 
-# Choose downloads
+// Choose downloads
 user_pref("browser.download.useDownloadDir", false);
 
-# Disable useless stuffs
+// Disable useless stuffs
 user_pref("signon.rememberSignons", false);
 user_pref("browser.search.update", false);
 user_pref("browser.tabs.warnOnClose", false);
 user_pref("browser.toolbarbuttons.introduced.pocket-button", true);
 user_pref("experiments.activeExperiment", false);
 
-# Homepage
+// Homepage
 user_pref("browser.startup.homepage", "https://prologin.org/");
 
-# Don't open "welcome" tabs
+// Don't open "welcome" tabs
 user_pref("browser.startup.homepage_override.mstone", "ignore");
 
-# UI buttons
+// UI buttons
 user_pref("browser.uiCustomization.state", "{\"placements\":{\"PanelUI-contents\":[\"edit-controls\",\"zoom-controls\",\"new-window-button\",\"privatebrowsing-button\",\"find-button\",\"downloads-button\",\"history-panelmenu\",\"preferences-button\"],\"addon-bar\":[\"addonbar-closebutton\",\"status-bar\"],\"PersonalToolbar\":[\"personal-bookmarks\"],\"nav-bar\":[\"urlbar-container\",\"home-button\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"toolbar-menubar\":[\"menubar-items\"]},\"seen\":[],\"dirtyAreaCache\":[\"PersonalToolbar\",\"nav-bar\",\"TabsToolbar\",\"toolbar-menubar\",\"PanelUI-contents\",\"addon-bar\"],\"currentVersion\":4,\"newElementCount\":0}");
 
-# Disable reporting bullshit
+// Disable reporting bullshit
 user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
 user_pref("toolkit.telemetry.prompted", 2);
 user_pref("toolkit.telemetry.rejected", true);
@@ -33,4 +33,3 @@ user_pref("datareporting.healthreport.logging.consoleEnabled", false);
 user_pref("datareporting.policy.dataSubmissionEnabled", false);
 user_pref("datareporting.policy.dataSubmissionPolicyResponseType", "accepted-info-bar-dismissed");
 user_pref("datareporting.policy.dataSubmissionPolicyAccepted", false);
-
