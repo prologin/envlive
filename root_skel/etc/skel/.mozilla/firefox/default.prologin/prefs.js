9c001a589134eab3fe02479a4681d968c3cec644
erdnaxe <erdnaxe@users.noreply.github.com>
2020-03-27 06:26:43 +0000

    skel: update firefox profile
    skel/firefox: user profile should be `default.prologin` rather than `prologin.default`.
    skel/firefox Use real JavaScript comments
    skel/firefox: Update default prefs
    skel/firefox: Use a private window, drop all deprecated configs and hide pocket.
    readme: Add testing how-to for Firefox

--- root_skel/etc/skel/.mozilla/firefox/default.prologin/prefs.js
+++ root_skel/etc/skel/.mozilla/firefox/default.prologin/prefs.js
@@ -1,36 +1,28 @@
-# Hide some prompts
-user_pref("browser.customizemode.tip0.shown", true);
+// Hide some prompts
 user_pref("reader.parse-on-load.enabled", false);
 
-# Choose downloads
+// Choose downloads
 user_pref("browser.download.useDownloadDir", false);
 
-# Disable useless stuffs
-user_pref("signon.rememberSignons", false);
+// Disable useless stuffs
 user_pref("browser.search.update", false);
 user_pref("browser.tabs.warnOnClose", false);
-user_pref("browser.toolbarbuttons.introduced.pocket-button", true);
-user_pref("experiments.activeExperiment", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("browser.newtabpage.enabled", false);
 
-# Homepage
+// Homepage
 user_pref("browser.startup.homepage", "https://prologin.org/");
 
-# Don't open "welcome" tabs
+// Don't open "welcome" tabs
 user_pref("browser.startup.homepage_override.mstone", "ignore");
 
-# UI buttons
+// UI buttons
 user_pref("browser.uiCustomization.state", "{\"placements\":{\"PanelUI-contents\":[\"edit-controls\",\"zoom-controls\",\"new-window-button\",\"privatebrowsing-button\",\"find-button\",\"downloads-button\",\"history-panelmenu\",\"preferences-button\"],\"addon-bar\":[\"addonbar-closebutton\",\"status-bar\"],\"PersonalToolbar\":[\"personal-bookmarks\"],\"nav-bar\":[\"urlbar-container\",\"home-button\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"toolbar-menubar\":[\"menubar-items\"]},\"seen\":[],\"dirtyAreaCache\":[\"PersonalToolbar\",\"nav-bar\",\"TabsToolbar\",\"toolbar-menubar\",\"PanelUI-contents\",\"addon-bar\"],\"currentVersion\":4,\"newElementCount\":0}");
 
-# Disable reporting bullshit
-user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
-user_pref("toolkit.telemetry.prompted", 2);
-user_pref("toolkit.telemetry.rejected", true);
-user_pref("toolkit.telemetry.enabled", false);
-user_pref("datareporting.healthreport.service.enabled", false);
+// Disable reporting bullshit
+// See https://firefox-source-docs.mozilla.org/toolkit/components/telemetry/internals/preferences.html
 user_pref("datareporting.healthreport.uploadEnabled", false);
-user_pref("datareporting.healthreport.service.firstRun", false);
-user_pref("datareporting.healthreport.logging.consoleEnabled", false);
 user_pref("datareporting.policy.dataSubmissionEnabled", false);
-user_pref("datareporting.policy.dataSubmissionPolicyResponseType", "accepted-info-bar-dismissed");
-user_pref("datareporting.policy.dataSubmissionPolicyAccepted", false);
 
+// Use a private browser window
+user_pref("browser.privatebrowsing.autostart", true);
