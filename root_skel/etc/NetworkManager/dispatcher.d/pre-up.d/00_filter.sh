fd061da4dcd21ce044c3fc34d45dbd625f3f9722
Neirpyc <cyprien.augier@protonmail.com>
2020-10-06 15:27:10 +0200

    Use long IP format
    Co-authored-by: risson <18313093+rissson@users.noreply.github.com>

--- root_skel/etc/NetworkManager/dispatcher.d/pre-up.d/00_filter.sh
+++ root_skel/etc/NetworkManager/dispatcher.d/pre-up.d/00_filter.sh
@@ -7,5 +7,5 @@ subnet="$(ip -o a show dev "$device" scope global | awk '{ print $4 }')"
 iptables -F
 
 iptables -A OUTPUT -d "$subnet" -j ACCEPT
-iptables -A OUTPUT -d 127/8 -j ACCEPT
+iptables -A OUTPUT -d 127.0.0.0/8 -j ACCEPT
 iptables -P OUTPUT DROP
